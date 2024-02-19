a31ca30135e4a66904e8d92fe1a6cbd620f47b7c
Kyle Barr <kylebarr54321@gmail.com>
2024-02-18 23:09:40 -0700

    move to API folder, so we can make frontend

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,5 @@
 import dotenv from "dotenv";
-import { diffString } from "json-diff";
+import { diff } from "json-diff";
 import { ReadDirectory } from "./helpers/ReadDirectory";
 
 dotenv.config();
@@ -8,6 +8,6 @@ let lastDirectory;
 
 setInterval(async () => {
     const newDirectory = await ReadDirectory(process.env.DIRECTORY);
-    if (lastDirectory) console.log(diffString(lastDirectory, newDirectory));
+    if (lastDirectory) console.log(diff(lastDirectory, newDirectory));
     lastDirectory = newDirectory;
-}, 1000 * 5);
+}, 1000 * 3);
